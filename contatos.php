b85ba3ca4d74100de0fd814cf050a42c6118f25b
alanmulero <alan.mulero@gmail.com>
2025-11-15 16:49:27 -0300

    Primeiro commit

---
+++ contatos.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'templates/header.php';
+
+
+?>
+
+<!doctype html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contatos</title>
+</head>
+<body>
+<h1>Contatos</h1>
+
+ <footer>
+        <?php include_once 'templates/footer.php'; ?>
+    </footer>
+</body>
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,26 @@
+<?php 
+include_once 'helpers/url.php';
+include_once 'templates/header.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Php do Blog Codar</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Fontes dp Google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<footer>
+    <p>Hora de codar &Copf; 2025</p>
+</footer>
+    
+</body>
+</html>
